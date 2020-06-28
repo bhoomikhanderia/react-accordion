620271ed44d0aa750cbb8575d136d88090b1d7bf
Bhoomi Khanderia <khanderiabhoomi@gmail.com>
2020-06-28 18:03:31 +1000

    setup store

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,7 +3,7 @@ import Logo from '../../logo.png';
 
 const NavBar = () => (
     <nav>
-        <a className="navbar-brand" href="/"><img src={Logo} alt="aesop logo"/></a>
+        <a className="nav-brand" href="/"><img src={Logo} alt="aesop logo"/></a>
     </nav>
 )
 
